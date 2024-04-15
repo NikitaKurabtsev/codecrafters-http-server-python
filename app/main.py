c5eb5490e7c246099accc5aad160a5f672b4262a
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 12:58:18 +0200

    pass 4nd stage

--- app/main.py
+++ app/main.py
@@ -7,21 +7,20 @@ def main():
 
     data = conn.recv(1024)
 
-    try:
-        http_method, path, http_version, *rest = data.split(b" ")
-        print(path)
+    http_method, path, http_version, *rest = data.split(b" ")
+    print(path)
 
-        if path == b"/":
-            response = "HTTP/1.1 200 OK\r\n\r\n"
-        elif path.startswith(b"/echo/"):
-            body = path.lstrip("/echo/")
-            response = (
-                f"HTTP/1.1 200 OK\r\n"
-                f"Content-Type: text/plain\r\n"
-                f"Content-Length: {len(body)}\r\n\r\n"
-                f"{body}"
-            )
-    except Exception as e:
+    if path == b"/":
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    elif path.startswith(b"/echo/"):
+        body = path.lstrip("/echo/")
+        response = (
+            f"HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/plain\r\n"
+            f"Content-Length: {len(body)}\r\n\r\n"
+            f"{body}"
+        )
+    else:
         print(f"Something goes wrong: {e}")
         response = "HTTP/1.1 400 Bad Request\r\n\r\n"
 
