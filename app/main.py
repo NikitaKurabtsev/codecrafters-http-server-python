a70274b37e37ade5f4ee92f1e808825e0edfd15d
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 09:52:36 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
     conn, host = server_socket.accept()
 
     data = conn.recv(4)
