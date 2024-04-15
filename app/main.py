730e5ea3c0515cc04e006d6cc76388512c005d44
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 12:55:20 +0200

    pass 4nd stage

--- app/main.py
+++ app/main.py
@@ -5,11 +5,6 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, host = server_socket.accept()
 
-    HTTP_200 = bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8")
-    HTTP_400 = bytes("HTTP/1.1 400 Bad Request\r\n\r\n", "utf-8")
-    HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
-    CONTENT_TYPE = bytes("Content-Type: text/plain\r\n", "utf-8")
-
     data = conn.recv(1024)
 
     try:
@@ -17,20 +12,20 @@ def main():
         print(path)
 
         if path == b"/":
-            conn.sendall(HTTP_200)
-        elif path == b"/echo/":
-            body = path.lstrip(b"/echo/")
-            print(body.encode())
-            CONTENT_LENGTH = bytes(f"Content-Length: {len(body)}\r\n\r\n")
-            response = f"{HTTP_200}{CONTENT_TYPE}{CONTENT_LENGTH}{body}"
-            conn.sendall(response)
-        else:
-            conn.sendall(HTTP_404)
-
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif path.startswith("/echo/"):
+            body = path.lstrip("/echo/")
+            response = (
+                f"HTTP/1.1 200 OK\r\n"
+                f"Content-Type: text/plain\r\n"
+                f"Content-Length: {len(body)}\r\n\r\n"
+                f"{body}"
+            )
     except Exception as e:
         print(f"Something goes wrong: {e}")
-        conn.sendall(HTTP_400)
+        response = "HTTP/1.1 400 Bad Request\r\n\r\n"
 
+    conn.sendall(response.encode())
     print(f"Connection from: {host}, received: {data}")
 
 if __name__ == "__main__":
