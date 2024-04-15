e8976e747f75a5102ab4306ef9c122a55b6bd3af
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 15:27:58 +0200

    pass 5nd stage

--- app/main.py
+++ app/main.py
@@ -1,45 +1,52 @@
 import socket
+from typing import List
+
+HTTP_200 = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
+HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
+CONTENT_TYPE = bytes("Content-Type: text/plain\r\n", "utf-8")
+CONTENT_LENGTH = bytes("Content-Length: ", "utf-8")
+
+
+def generate_response(content: bytes) -> bytes:
+    content_length = len(content).to_bytes()
+    return (
+        HTTP_200
+        + CONTENT_TYPE
+        + CONTENT_LENGTH
+        + content_length
+        + b"\r\n\r\n"
+        + content
+    )
+
+
+def process_request(path: bytes, headers: List[bytes]) -> bytes:
+    match path:
+        case b"/":
+            response = HTTP_200 + b"\r\n"
+        case path.startswith(b"/echo/"):
+            content = path.lstrip(b"/echo/")
+            response = generate_response(content)
+        case path.startswith(b"/user-agent"):
+            content = headers[2].split(b" ")[-1]
+            response = generate_response(content)
+        case _:
+            response = HTTP_404
+
+    return response
 
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, host = server_socket.accept()
 
-    HTTP_200 = bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8")
-    HTTP_200_ONE_LINE = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
-    HTTP_400 = bytes("HTTP/1.1 400 Bad Request\r\n\r\n", "utf-8")
-    HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
-    CONTENT_TYPE = bytes("Content-Type: text/plain\r\n", "utf-8")
-
     data = conn.recv(1024)
 
-    try:
-        http_method, path, http_version, *rest = data.split(b" ")
-        print(path)
-
-        if path == b"/":
-            response = HTTP_200
-        elif path.startswith(b"/echo/"):
-            content = path.lstrip(b"/echo/")
-            print(content)
-            content_length = str(len(content))
-            response = (
-                    HTTP_200_ONE_LINE
-                    + CONTENT_TYPE
-                    + b"Content-Length: "
-                    + bytes(content_length, "utf-8")
-                    + b"\r\n\r\n"
-                    + content
-            )
-        else:
-            response = HTTP_404
-
-    except Exception as e:
-        print(f"Something goes wrong: {e}")
-        response = HTTP_400
+    http_headers = data.split(b"\r\n")
+    path = data.split(b" ")[1]
+    response = process_request(path, http_headers)
 
     conn.sendall(response)
-    print(f"Connection from: {host}, received: {data}")
+
 
 if __name__ == "__main__":
     main()
