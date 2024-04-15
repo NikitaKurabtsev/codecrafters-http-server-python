1472aac0a9e3b33879504510a1b50b1af17f6b5c
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 09:48:49 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
+    conn, host = server_socket.accept()
 
+    data = conn.recv(4)
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    print(f"Connection from: {host}, recieved: {data}")
 
 if __name__ == "__main__":
     main()
