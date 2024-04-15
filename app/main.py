3a320b213dc40cb88d09918cd04de7e68f49057b
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 12:29:10 +0200

    pass 4nd stage

--- app/main.py
+++ app/main.py
@@ -5,23 +5,22 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, host = server_socket.accept()
 
-    HTTP_200 = bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8")
+    HTTP_200 = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
     HTTP_400 = bytes("HTTP/1.1 400 Bad Request\r\n\r\n", "utf-8")
     HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
-    CONTENT_TYPE = bytes("Content-Type: text/plain\r\n\r\n", "utf-8")
+    CONTENT_TYPE = bytes("Content-Type: text/plain\r\n", "utf-8")
 
     data = conn.recv(1024)
 
     try:
         http_method, path, http_version, *rest = data.split(b" ")
         print(path)
-        message = path.split(b"/")
-        message = f"{message[1]}\r\n\r\n"
-        print(message)
-        CONTENT_LENGTH = bytes(f"Content-Length: {len(message)}\r\n\r\n", "utf-8")
-        HTTP_MESSAGE =  f"{HTTP_200} {CONTENT_TYPE}, {CONTENT_LENGTH} {message}"
-        if path == b"/":
-            conn.sendall(HTTP_MESSAGE)
+
+        if path == b"/echo/":
+            body = path.lstrip("/echo/")
+            CONTENT_LENGTH = bytes(f"Content-Length: {len(body)}\r\n\r\n")
+            response = f"{HTTP_200}{CONTENT_TYPE}{CONTENT_LENGTH}{body}"
+            conn.sendall(response)
         else:
             conn.sendall(HTTP_404)
 
