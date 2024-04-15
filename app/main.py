96c2365597bff8ecaf3136d0134af8bff8ac219a
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-15 10:27:53 +0200

    pass 3nd stage

--- app/main.py
+++ app/main.py
@@ -2,15 +2,29 @@ import socket
 
 
 def main():
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, host = server_socket.accept()
 
-    data = conn.recv(4)
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    HTTP_200 = bytes("HTTP/1.1 200 OK\r\n\r\n")
+    HTTP_400 = bytes("HTTP/1.1 400 Bad Request\r\n\r\n")
+    HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n")
 
-    print(f"Connection from: {host}, recieved: {data}")
+    data = conn.recv(1024)
+
+    try:
+        http_method, path, http_version, *rest = data.split(b" ")
+        print(path)
+
+        if path == b"/":
+            conn.sendall(HTTP_200)
+        else:
+            conn.sendall(HTTP_404)
+
+    except Exception as e:
+        print(f"Something goes wrong: {e}")
+        conn.sendall(HTTP_400)
+
+    print(f"Connection from: {host}, received: {data}")
 
 if __name__ == "__main__":
     main()
