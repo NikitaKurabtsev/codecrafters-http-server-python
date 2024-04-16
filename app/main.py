f75f5360a05861cfafb4996d34ad2933f02c24cb
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-16 12:47:17 +0200

    pass 7nd stage

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 import socket
+import sys
+import os
 from typing import List
 from threading import Thread
 
 HTTP_200 = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
 HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
-CONTENT_TYPE = bytes("Content-Type: text/plain\r\n", "utf-8")
+CONTENT_TYPE_TEXT = bytes("Content-Type: text/plain\r\n", "utf-8")
+CONTENT_TYPE_APP = bytes("Content-Type: application/octet-stream\r\n", "utf-8")
 CONTENT_LENGTH = bytes("Content-Length: ", "utf-8")
 
 
-def generate_response(content: bytes) -> bytes:
+def generate_response(content: bytes, content_type: bytes) -> bytes:
     content_length = str(len(content))
     return (
         HTTP_200
-        + CONTENT_TYPE
+        + content_type
         + CONTENT_LENGTH
         + bytes(content_length, "utf-8")
         + b"\r\n\r\n"
@@ -21,16 +24,29 @@ def generate_response(content: bytes) -> bytes:
 
 
 def process_request(path: bytes, headers: List[bytes]) -> bytes:
+    response = None
+
     match path:
         case b"/":
             response = HTTP_200 + b"\r\n"
         case _ if path.startswith(b"/echo/"):
             content = path.lstrip(b"/echo/")
-            response = generate_response(content)
+            response = generate_response(content, CONTENT_TYPE_TEXT)
         case _ if path.startswith(b"/user-agent"):
             content = headers[2].split(b" ")[-1]
-            response = generate_response(content)
-        case _:
+            response = generate_response(content, CONTENT_TYPE_TEXT)
+        case _ if path.startswith(b"/files/") and sys.argv[1] == "--directory":
+            filename = path.lstrip(b"/files/")
+            directory = sys.argv[1]
+            filepath = os.path.join(directory, str(filename))
+            match filepath:
+                case _ if os.path.exists(filepath):
+                    with open(filepath, 'rb') as file:
+                        content = file.read()
+                        response = generate_response(content, CONTENT_TYPE_APP)
+                case _:
+                    response = HTTP_404
+        case _ if os.path:
             response = HTTP_404
 
     return response
