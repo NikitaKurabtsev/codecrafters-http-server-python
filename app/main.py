3649679393ebedd0fd58c77807d8bd85b89a96d7
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-16 15:21:04 +0200

    pass 7nd stage

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ import os
 from typing import List
 from threading import Thread
 
-HTTP_200 = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
-HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n", "utf-8")
+HTTP_200 = bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8")
+HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
 
 
 def generate_response(content: bytes, file=False) -> bytes:
@@ -23,7 +23,7 @@ def generate_response(content: bytes, file=False) -> bytes:
 def process_request(path: bytes, headers: List[bytes]) -> bytes:
     match path:
         case b"/":
-            response = HTTP_200 + b"\r\n"
+            response = HTTP_200
         case _ if path.startswith(b"/echo/"):
             content = path.lstrip(b"/echo/")
             response = generate_response(content)
@@ -34,12 +34,15 @@ def process_request(path: bytes, headers: List[bytes]) -> bytes:
             filename = path.lstrip(b"/files/")
             directory = sys.argv[2]
             filepath = os.path.join(directory, filename.decode())
-            if os.path.exists(filepath):
-                with open(filepath, 'rb') as file:
-                    content = file.read()
-                    response = generate_response(content, file=True)
-            else:
-                response = HTTP_404
+            match filepath:
+                case _ if os.path.exists(filepath):
+                    with open(filepath, 'rb') as file:
+                        content = file.read()
+                        response = generate_response(content, file=True)
+                case _ if not os.path.exists(filepath):
+                    response = HTTP_404 + b"Content-length: 0\r\n\r\n"
+                case _:
+                    response = HTTP_404
         case _:
             response = HTTP_404
 
