2dfd7f1e7bef3351be7bf2fead80d5e61aae5092
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-17 17:26:58 +0300

    8 stage

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from typing import List
 from threading import Thread
 
 HTTP_200 = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
+HTTP_201 = bytes("HTTP/1.1 201 Created\r\n\r\n", "utf-8")
 HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
 
 
@@ -13,14 +14,18 @@ def generate_response(content: bytes, file=False) -> bytes:
 
     return (
         HTTP_200
-        + (b"Content-Type: application/octet-stream\r\n" if file else b"Content-Type: text/plain\r\n")
+        + (
+            b"Content-Type: application/octet-stream\r\n"
+            if file
+            else b"Content-Type: text/plain\r\n"
+        )
         + (b"Content-Length: " + content_length)
         + b"\r\n\r\n"
         + content
     )
 
 
-def process_request(path: bytes, headers: List[bytes]) -> bytes:
+def process_request(request_data: List[bytes], http_method: bytes, path: bytes) -> bytes:
     match path:
         case b"/":
             response = HTTP_200 + b"\r\n"
@@ -28,7 +33,7 @@ def process_request(path: bytes, headers: List[bytes]) -> bytes:
             content = path.lstrip(b"/echo/")
             response = generate_response(content)
         case _ if path.startswith(b"/user-agent"):
-            content = headers[2].split(b" ")[-1]
+            content = request_data[2].split(b" ")[-1]
             response = generate_response(content)
         case _ if path.startswith(b"/files/") and sys.argv[1] == "--directory":
             filename = path.lstrip(b"/files/")
@@ -36,11 +41,20 @@ def process_request(path: bytes, headers: List[bytes]) -> bytes:
             filepath = os.path.join(directory, filename.decode())
             match filepath:
                 case _ if os.path.exists(filepath):
-                    with open(filepath, 'rb') as file:
-                        content = file.read()
-                        response = generate_response(content, file=True)
+                    if http_method == b"GET":
+                        with open(filepath, "rb") as file:
+                            content = file.read()
+                            response = generate_response(content, file=True)
+                    if http_method == b"POST":
+                        with open(filepath, "wb", encoding="utf-8") as file:
+                            content = request_data[-1]
+                            file.write(content)
+                            response = HTTP_201
+                    else:
+                        response = HTTP_404
                 case _:
                     response = HTTP_404[:-2] + b"Content-Length: 0\r\n\r\n"
+
         case _:
             response = HTTP_404
 
@@ -49,11 +63,13 @@ def process_request(path: bytes, headers: List[bytes]) -> bytes:
 
 def handle_connection(client_connection: socket.socket) -> None:
     data = client_connection.recv(1024)
+    print(data)
 
-    http_headers = data.split(b"\r\n")
+    http_method = data.split(b" ")[0]
+    request_data = data.split(b"\r\n")
     path = data.split(b" ")[1]
 
-    response = process_request(path, http_headers)
+    response = process_request(request_data, http_method, path)
 
     client_connection.sendall(response)
 
