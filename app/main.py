4542530d32571b558072cf74104b97eb9bdc7939
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-18 10:39:16 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -4,9 +4,18 @@ import os
 from typing import List
 from threading import Thread
 
+SERVER_HOST = "localhost"
+SERVER_PORT = 4221
+
 HTTP_200 = bytes("HTTP/1.1 200 OK\r\n", "utf-8")
 HTTP_201 = bytes("HTTP/1.1 201 Created\r\n\r\n", "utf-8")
+HTTP_400 = bytes("HTTP/1.1 400 Bad Request\r\n\r\n", "utf-8")
 HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
+
+GET = bytes("GET")
+POST = bytes("POST")
+
+MAX_BUFFER_SIZE = 1024
 
 
 def generate_response(content: bytes, file: bool = False) -> bytes:
@@ -39,7 +48,7 @@ def process_request(request_data: List[bytes], http_method: bytes, path: bytes) 
         directory = sys.argv[2]
         filepath = os.path.join(directory, filename.decode())
 
-        if http_method == b"GET":
+        if http_method == GET:
             if os.path.exists(filepath):
                 with open(filepath, "rb") as file:
                     content = file.read()
@@ -47,13 +56,13 @@ def process_request(request_data: List[bytes], http_method: bytes, path: bytes) 
             else:
                 response = HTTP_404[:-2] + b"Content-Length: 0\r\n\r\n"
 
-        elif http_method == b"POST":
+        elif http_method == POST:
             with open(filepath, "wb") as file:
                 content = request_data[-1]
                 file.write(content)
                 response = HTTP_201
         else:
-            response = HTTP_404
+            response = HTTP_400
     else:
         response = HTTP_404
 
@@ -61,7 +70,7 @@ def process_request(request_data: List[bytes], http_method: bytes, path: bytes) 
 
 
 def handle_connection(client_connection: socket.socket) -> None:
-    data = client_connection.recv(1024)
+    data = client_connection.recv(MAX_BUFFER_SIZE)
 
     http_method = data.split(b" ")[0]
     request_data = data.split(b"\r\n")
@@ -73,11 +82,10 @@ def handle_connection(client_connection: socket.socket) -> None:
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server((SERVER_HOST, SERVER_PORT), reuse_port=True)
 
     while True:
         client_socket = server_socket.accept()[0]
-
         thread = Thread(target=handle_connection, args=[client_socket])
         thread.start()
 
