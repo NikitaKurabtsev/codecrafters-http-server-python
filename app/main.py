9fad7bfe8ef7a862608fca5533f864b740d1db8f
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-18 10:41:35 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ HTTP_201 = bytes("HTTP/1.1 201 Created\r\n\r\n", "utf-8")
 HTTP_400 = bytes("HTTP/1.1 400 Bad Request\r\n\r\n", "utf-8")
 HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
 
-GET = bytes("GET")
-POST = bytes("POST")
+GET = bytes("GET", "utf-8")
+POST = bytes("POST", "utf-8")
 
 MAX_BUFFER_SIZE = 1024
 
