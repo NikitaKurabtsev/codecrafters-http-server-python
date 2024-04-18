0ca9a07a6be5db2e5a26570349f9bf765569e693
NikitaKurabtsev <nick3500hunna@gmail.com>
2024-04-18 10:21:48 +0200

    refactor

--- app/main.py
+++ app/main.py
@@ -9,16 +9,13 @@ HTTP_201 = bytes("HTTP/1.1 201 Created\r\n\r\n", "utf-8")
 HTTP_404 = bytes("HTTP/1.1 404 Not Found\r\n\r\n", "utf-8")
 
 
-def generate_response(content: bytes, file=False) -> bytes:
+def generate_response(content: bytes, file: bool = False) -> bytes:
     content_length = str(len(content)).encode("utf-8")
+    content_type = b"application/octet-stream" if file else b"text/plain"
 
     return (
         HTTP_200
-        + (
-            b"Content-Type: application/octet-stream\r\n"
-            if file
-            else b"Content-Type: text/plain\r\n"
-        )
+        + (b"Content-Type: " + content_type + b"\r\n")
         + (b"Content-Length: " + content_length)
         + b"\r\n\r\n"
         + content
@@ -26,34 +23,39 @@ def generate_response(content: bytes, file=False) -> bytes:
 
 
 def process_request(request_data: List[bytes], http_method: bytes, path: bytes) -> bytes:
-    match path:
-        case b"/":
-            response = HTTP_200 + b"\r\n"
-        case _ if path.startswith(b"/echo/"):
-            content = path.lstrip(b"/echo/")
-            response = generate_response(content)
-        case _ if path.startswith(b"/user-agent"):
-            content = request_data[2].split(b" ")[-1]
-            response = generate_response(content)
-        case _ if path.startswith(b"/files/") and sys.argv[1] == "--directory":
-            filename = path.lstrip(b"/files/")
-            directory = sys.argv[2]
-            filepath = os.path.join(directory, filename.decode())
-            match filepath:
-                case _ if os.path.exists(filepath) and http_method == b"GET":
-                    with open(filepath, "rb") as file:
-                        content = file.read()
-                        response = generate_response(content, file=True)
-                case _ if http_method == b"POST":
-                    with open(filepath, "wb") as file:
-                        content = request_data[-1]
-                        file.write(content)
-                        response = HTTP_201
-                case _:
-                    response = HTTP_404[:-2] + b"Content-Length: 0\r\n\r\n"
+    if path == b"/":
+        response = HTTP_200 + b"\r\n"
 
-        case _:
+    elif path.startswith(b"/echo/"):
+        content = path.lstrip(b"/echo/")
+        response = generate_response(content)
+
+    elif path.startswith(b"/user-agent"):
+        content = request_data[2].split(b" ")[-1]
+        response = generate_response(content)
+
+    elif path.startswith(b"/files/") and sys.argv[1] == "--directory":
+        filename = path.lstrip(b"/files/")
+        directory = sys.argv[2]
+        filepath = os.path.join(directory, filename.decode())
+
+        if http_method == b"GET":
+            if os.path.exists(filepath):
+                with open(filepath, "rb") as file:
+                    content = file.read()
+                    response = generate_response(content, file=True)
+            else:
+                response = HTTP_404[:-2] + b"Content-Length: 0\r\n\r\n"
+
+        elif http_method == b"POST":
+            with open(filepath, "wb") as file:
+                content = request_data[-1]
+                file.write(content)
+                response = HTTP_201
+        else:
             response = HTTP_404
+    else:
+        response = HTTP_404
 
     return response
 
